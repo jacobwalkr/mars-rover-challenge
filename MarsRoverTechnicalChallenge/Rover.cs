e29545818d8b1f9a10550b82ea554904657543e9
Jacob Walker <jacob.walker94+github@gmail.com>
2013-07-18 23:48:34 +0100

    Wrote skeleton for Rover class

--- MarsRoverTechnicalChallenge/Rover.cs
+++ MarsRoverTechnicalChallenge/Rover.cs
@@ -6,10 +6,17 @@ namespace MarsRoverTechnicalChallenge
     class Rover
     {
         private Point start;
+        private Point limit;
 
-        public Rover(Point start)
+        public Rover(Point start, Point limit)
         {
             this.start = start;
+            this.limit = limit;
+        }
+
+        public Point Go(string instructions)
+        {
+            throw new NotImplementedException();
         }
     }
 }
