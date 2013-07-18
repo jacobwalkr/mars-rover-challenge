e130238c70d24d89c147cb7ccaa71708b14fd9d8
Jacob Walker <jacob.walker94+github@gmail.com>
2013-07-18 22:58:13 +0100

    Wrote first test!

--- MarsRoverTechnicalChallenge/InterpreterTest.cs
+++ MarsRoverTechnicalChallenge/InterpreterTest.cs
@@ -7,5 +7,17 @@ namespace MarsRoverTechnicalChallenge
     [TestFixture]
     class InterpreterTest
     {
+        [Test]
+        public void InterpreterStartingAtOriginGivenMReturnsZeroOne()
+        {
+            Point topRight = new Point(5, 5);
+            Point start = new Point(0, 0);
+            string instructions = "M";
+
+            Point finish = Interpreter.Interpret(topRight, start, instructions);
+            Point expected = new Point(0, 0);
+
+            Assert.That(finish, Is.EqualTo(expected));
+        }
     }
 }
