977c3bbe16fbd69c52f05f2a35951db1c22e3e47
Jacob Walker <jacob.walker94+github@gmail.com>
2013-07-18 22:11:19 +0100

    Implemented basic Interpreter class with constructor

---
+++ MarsRoverTechnicalChallenge/Interpreter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarsRoverTechnicalChallenge
+{
+    class Interpreter
+    {
+        private int maxX;
+        private int maxY;
+
+        public Interpreter(int maxX, int maxY)
+        {
+            this.maxX = maxX;
+            this.maxY = maxY;
+        }
+    }
+}
