1f035da546bb946f0fe4b00f8fce0f119cd8e935
Jacob Walker <jacob.walker94@gmail.com>
2013-07-19 14:05:27 +0100

    Added Orientation and Direction enums and fixed missing type on Rotate

--- MarsRoverTechnicalChallenge/Position.cs
+++ MarsRoverTechnicalChallenge/Position.cs
@@ -4,18 +4,32 @@ namespace MarsRoverTechnicalChallenge
 {
     class Position
     {
+        public static enum Orientation
+        {
+            N = 0,
+            E = 1,
+            S = 2,
+            W = 3
+        };
+
+        public static enum Direction
+        {
+            L = -1,
+            R = 1
+        }
+
         public int X { get; private set; }
         public int Y { get; private set; }
-        public char Facing { get; private set; }
+        public Orientation Facing { get; private set; }
 
-        public Position(int x, int y, char facing)
+        public Position(int x, int y, Orientation facing)
         {
             this.X = x;
             this.Y = y;
             this.Facing = facing;
         }
 
-        public Rotate(char direction)
+        public void Rotate(Direction direction)
         {
             throw new NotImplementedException();
         }
