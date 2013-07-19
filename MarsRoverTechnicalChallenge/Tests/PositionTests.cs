039c9f11c72147c8b54de4814a7c64df1519ac42
Jacob Walker <jacob.walker94@gmail.com>
2013-07-19 14:35:21 +0100

    First test for Position

---
+++ MarsRoverTechnicalChallenge/Tests/PositionTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace MarsRoverTechnicalChallenge.Tests
+{
+    [TestFixture]
+    class PositionTests
+    {
+        [Test]
+        public void PositionGivenZeroZeroNConstructsCorrectly()
+        {
+            var position = new Position(0, 0, Position.Orientation.N);
+
+            Assert.That(position.X, Is.EqualTo(0));
+            Assert.That(position.Y, Is.EqualTo(0));
+            Assert.That(position.Facing, Is.EqualTo(Position.Orientation.N));
+        }
+    }
+}
