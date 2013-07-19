d748834c60f8a918108c7aa99a72c7051ea54ef1
Jacob Walker <jacob.walker94@gmail.com>
2013-07-19 13:59:03 +0100

    Added Position class with the intention of representing coords and direction

---
+++ MarsRoverTechnicalChallenge/Position.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace MarsRoverTechnicalChallenge
+{
+    class Position
+    {
+        public int X { get; private set; }
+        public int Y { get; private set; }
+        public char Facing { get; private set; }
+
+        public Position(int x, int y, char facing)
+        {
+            this.X = x;
+            this.Y = y;
+            this.Facing = facing;
+        }
+
+        public Rotate(char direction)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
