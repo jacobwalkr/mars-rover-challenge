427fff213307616a9164196d3f9ee8dc0f40c822
Jacob Walker <jacob.walker94+github@gmail.com>
2013-07-20 19:36:03 +0100

    Split PositionTests into more granular classes

---
+++ MarsRoverTechnicalChallengeTests/PositionConstructorTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using MarsRoverTechnicalChallenge;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MarsRoverTechnicalChallengeTests
+{
+    [TestClass]
+    public class PositionConstructorTests
+    {
+        [TestMethod]
+        public void PositionGivenZeroZeroNConstructsCorrectly()
+        {
+            var position = new Position(0, 0, Orientation.N);
+
+            Assert.AreEqual(position.X, 0);
+            Assert.AreEqual(position.Y, 0);
+            Assert.AreEqual(Orientation.N, position.Facing);
+        }
+
+        [TestMethod]
+        public void PositionGivenFiveFiveSConstructsCorrectly()
+        {
+            var position = new Position(5, 5, Orientation.S);
+
+            Assert.AreEqual(position.X, 5);
+            Assert.AreEqual(position.Y, 5);
+            Assert.AreEqual(Orientation.S, position.Facing);
+        }
+    }
+}
